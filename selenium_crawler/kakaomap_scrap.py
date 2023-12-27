8603b2ce30e6dc1a74361b2d6883150685b43d11
riseryan89 <rise.ryan.lee@gmail.com>
2023-12-27 11:14:52 +0900

    kakaomap 크롤링

--- selenium_crawler/kakaomap_scrap.py
+++ selenium_crawler/kakaomap_scrap.py
@@ -24,17 +24,66 @@ def get_data_from_kakaomap():
         search_input.send_keys("강남구 카페")
         search_input.send_keys(Keys.ENTER)
         wait = WebDriverWait(driver, 10)
-        wait.until(ec.visibility_of_element_located((By.ID, "info.search.place.list")))
+        wait.until(ec.element_to_be_clickable((By.ID, "info.search.place.more")))
+        driver.execute_script("""
+        var element = document.getElementById('dimmedLayer');
+        if (element) {
+            element.className = 'DimmedLayer HIDDEN';
+        }
+        """)
 
+        sleep(1)
+        show_more_btn = driver.find_element(By.ID, "info.search.place.more")
+        show_more_btn.click()
+
+        wait = WebDriverWait(driver, 10)
+        wait.until(ec.visibility_of_element_located((By.ID, "info.search.page")))
+
+        page_count = 0
+        items = []
+
+        while page_count <= 5:
+            if page_count != 0 and page_count % 5 == 0:
+                page_next_btn_id = "info.search.page.next"
+                next_btn = driver.find_element(By.ID, page_next_btn_id)
+                next_btn.click()
+                wait = WebDriverWait(driver, 10)
+                wait.until(ec.visibility_of_element_located((By.ID, "info.search.place.list")))
+
+            page_count += 1
+            page_num = page_count % 5 if page_count % 5 != 0 else 5
+      
+            page_btn_id = f"info.search.page.no{page_num}"
+            next_btn = driver.find_element(By.ID, page_btn_id)
+            next_btn.click()
+            wait = WebDriverWait(driver, 10)
+            wait.until(ec.visibility_of_element_located((By.ID, "info.search.place.list")))
+
+            place_list = driver.find_element(By.ID, "info.search.place.list")
+            shop_list = place_list.get_attribute("innerHTML")
+
+            get_items(shop_list, items)
+            sleep(2)
         # 검색 결과 확인
-        place_list = driver.find_element(By.ID, "info.search.place.list")
-        shop_list = place_list.get_attribute("innerHTML")
-
-        # scroll down to the bottom
+        
         driver.quit()
-        return shop_list
+        return items
     except Exception as e:
         print(e)
         raise e
     
     
+def get_items(html: str, parsed_items: list):
+    from bs4 import BeautifulSoup
+
+    soup = BeautifulSoup(html, "html.parser")
+    items = soup.select("li.PlaceItem.clickArea")
+    for item in items:
+        item_dict = {}
+        item_dict["name"] = item.find('span', {'data-id': 'screenOutName'}).text
+        item_dict["score"] = item.find('em', {'data-id': 'scoreNum'}).text
+        item_dict["address"] = item.find('p', {'data-id': 'address'}).text
+        item_dict["hour"] = item.find('a', {'data-id': 'periodTxt'}).text
+        parsed_items.append(item_dict)
+
+    return parsed_items
