e3c49ec53e0b5ca2f20933c57a39abeb69606c53
riseryan89 <rise.ryan.lee@gmail.com>
2024-02-13 01:53:02 +0900

    배포

--- last_chapter/model_creator/lambda_function.py
+++ last_chapter/model_creator/lambda_function.py
@@ -8,7 +8,7 @@ from sklearn.ensemble import RandomForestClassifier
 
 def load_and_preprocess_data():
     file_s3 = boto3.client('s3')
-    file_s3.download_file('fc-python-package', 'weather_data_updated.csv', 'weather_data_updated.csv')
+    file_s3.download_file('fc-python-online', 'weather_data_updated.csv', 'weather_data_updated.csv')
     data = pd.read_csv('weather_data_updated.csv')
 
 
@@ -41,7 +41,7 @@ def train_and_predict(X, y):
 
     return model
 
-def create_model():
+def lambda_handler(event, context):
     data = load_and_preprocess_data()   
     # 이상치 제거
     for column in data.select_dtypes(include=['float64', 'int64']).columns:
@@ -58,7 +58,14 @@ def create_model():
 
     X_processed = preprocessor.fit_transform(X)
     model = train_and_predict(X_processed, y)
-    s3 = boto3.resource('s3')
-    s3.Bucket('fc-python-package').put_object(Key='model_joblib.pkl', Body=joblib.dumps(model))
+
+    # S3 클라이언트 생성
+    s3_client = boto3.client('s3')
+
+    # 모델 데이터를 직렬화
+    model_data = joblib.dumps(model)
+
+    # S3 버킷에 객체 저장
+    s3_client.put_object(Bucket='fc-python-online', Key='model_joblib.pkl', Body=model_data)
 
     return {}
