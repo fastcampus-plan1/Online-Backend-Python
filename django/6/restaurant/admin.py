ba5feb89c0a3c0040540236a48df57b884f43d1a
MS Seo <clichedmoog@gmail.com>
2024-06-03 01:40:46 +0900

    6 코드 업데이트

--- django/6/restaurant/admin.py
+++ django/6/restaurant/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 from restaurant.models import Restaurant
 
+# 기본 어드민을 사용할 때 모델만 등록
+#admin.site.register(Restaurant)
 
 class RestaurantAdmin(admin.ModelAdmin):
     list_display = [
@@ -12,7 +14,7 @@ class RestaurantAdmin(admin.ModelAdmin):
         "created_at",
         "updated_at",
     ]
-    search_fields = ["name", "phone"]
+    fields = ["name", "phone", "rating"]
 
-
+# 모델 어드민 클래스를 등록할 때는 모델과 모델 어드민 클래스를 함께 등록
 admin.site.register(Restaurant, RestaurantAdmin)
